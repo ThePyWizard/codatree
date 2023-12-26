a15109433f91555e85d7a243e21f09d121c84b09
Jishnu M M <jishnumm4312@gmail.com>
2023-12-26 09:57:09 +0530

    made the basic 3d structure of the tree

--- sketch.js
+++ sketch.js
@@ -1,12 +1,55 @@
+let img_tree;
+let img_wood;
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+
+  createCanvas(710, 400, WEBGL);
+  img_tree=loadImage('tree-texture.jpg')
+  img_wood=loadImage('wood-texture2.jpg')
+}
+
+function draw() {
+
+    //position light according to the cursor movement
+    let locX = mouseX - height / 2;
+    let locY = mouseY - width / 2;
+    ambientLight(60, 60, 60);
+    pointLight(255, 255, 255, locX, locY, 100);
+
+    //triangle creation
+    push()
+    translate(-100, -120, 0);
+    rotateY(frameCount * 0.01);
+    rotateX(PI);
+    texture(img_tree);
+    cone(40, 80);
+    pop()
+
+    //triangle creation
+    push()
+    translate(-100, -150, 0);
+    rotateY(frameCount * 0.01);
+    rotateX(PI);
+    texture(img_tree);
+    cone(20, 40);
+    pop()
+
+    //triangle creation
+    push()
+    translate(-100, -85, 0);
+    rotateY(frameCount * 0.01);
+    rotateX(PI);
+    texture(img_tree);
+    cone(60, 130);
+    pop()
+    
+    //rectangle creation
+    push();
+    translate(-100, -10, 0);
+    texture(img_wood);
+    box(20, 50, 0);
+    pop();
+    
+}
+
+
